71d62d397bf56bff66ef2cf6efb4073da66f3b8c
Атаманюк Вася <vasilatamaniuk@gmail.com>
2020-10-02 12:57:16 +0300

    Updates

---
+++ precache-manifest.141fd811ec1e4ce831e6bbb7aea8d45a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faac3f38ae33335fc8793bf9cf6c728e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ea1c92d49aef985f3f5b",
+    "url": "/weather-app/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "095ec92ce560198516c5",
+    "url": "/weather-app/static/css/main.07e1eb75.chunk.css"
+  },
+  {
+    "revision": "ea1c92d49aef985f3f5b",
+    "url": "/weather-app/static/js/2.08c45858.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.08c45858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095ec92ce560198516c5",
+    "url": "/weather-app/static/js/main.8ec5f58e.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/weather-app/static/media/getFetch.013f0b90.cjs"
+  }
+]);
