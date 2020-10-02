d80e6556b8218330200cc16f2dafea1e5bf393c9
Атаманюк Вася <vasilatamaniuk@gmail.com>
2020-10-02 13:28:39 +0300

    fixing images

--- src/components/App.js
+++ src/components/App.js
@@ -46,31 +46,31 @@ const App = (props) => {
 
     useEffect(() => {
         if (icon.icon === '01d' || icon.icon === '01n') {
-            setBackground('../images/clear-sky.jpg');
+            setBackground('http://aceofspadesss.github.io/weather-app/images/clear-sky.jpg');
             setGradient({ color1: 'rgba(86,77,241,0.9)', color2: 'rgba(80,80,246,0.8)', color3: 'rgba(219,249,255,0.7)' });
         } else if (icon.icon === '02d' || icon.icon === '02n') {
-            setBackground('../images/few-clouds.jpg');
+            setBackground('http://aceofspadesss.github.io/weather-app/images/few-clouds.jpg');
             setGradient({ color1: 'rgba(53,98,166,0.9)', color2: 'rgba(68,157,241,0.8)', color3: 'rgba(172,223,233,0.7)' });
         } else if (icon.icon === '03d' || icon.icon === '03n') {
-            setBackground('../images/scattered-clouds.jpg');
+            setBackground('http://aceofspadesss.github.io/weather-app/images/scattered-clouds.jpg');
             setGradient({ color1: 'rgba(136,136,136,0.9)', color2: 'rgba(113,145,175,0.8)', color3: 'rgba(230,230,230,0.7)' });
         } else if (icon.icon === '04d' || icon.icon === '04n') {
-            setBackground('../images/broken-clouds.jpg');
+            setBackground('http://aceofspadesss.github.io/weather-app/images/broken-clouds.jpg');
             setGradient({ color1: 'rgba(204,204,204,0.9)', color2: 'rgba(90,134,176,0.8)', color3: 'rgba(29,56,73,0.7)' });
         } else if (icon.icon === '09d' || icon.icon === '09n') {
-            setBackground('../images/shower-rain.jpg');
+            setBackground('http://aceofspadesss.github.io/weather-app/images/shower-rain.jpg');
             setGradient({ color1: 'rgba(149,177,255,0.9)', color2: 'rgba(40,70,98,0.8)', color3: 'rgba(0,0,0,0.7)' });
         } else if (icon.icon === '10d' || icon.icon === '10n') {
-            setBackground('../images/rain.jpg');
+            setBackground('http://aceofspadesss.github.io/weather-app/images/rain.jpg');
             setGradient({ color1: 'rgba(41,93,21,0.9)', color2: 'rgba(110,150,119,0.8)', color3: 'rgba(231,255,180,0.7)' });
         } else if (icon.icon === '11d' || icon.icon === '11n') {
-            setBackground('../images/thunderstorm.jpg');
+            setBackground('http://aceofspadesss.github.io/weather-app/images/thunderstorm.jpg');
             setGradient({ color1: 'rgba(0,5,45,0.9)' , color2: 'rgba(23,21,77,0.8)', color3: 'rgba(145,143,213,0.7)' });
         } else if (icon.icon === '13d' || icon.icon === '13n') {
-            setBackground('../images/snow.jpg');
+            setBackground('http://aceofspadesss.github.io/weather-app/images/snow.jpg');
             setGradient({ color1: 'rgba(162,170,195,0.9)', color2: 'rgba(233,233,233,0.8)', color3: 'rgba(238,238,238,0.7)' });
         } else if (icon.icon === '50d' || icon.icon === '50n') {
-            setBackground('../images/mist.jpg');
+            setBackground('http://aceofspadesss.github.io/weather-app/images/mist.jpg');
             setGradient({ color1: 'rgba(58,76,91,0.9)', color2: 'rgba(222,222,222,0.8)', color3: 'rgba(150,150,150,0.7)' });
         }
     }, [icon.description]);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,39 +19,39 @@ const Navbar = (props) => {
                         </a>
                         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                             <button className="dropdown-item" onClick={() => {
-                                                                props.setBackground('../images/clear-sky.jpg'); 
+                                                                props.setBackground('http://aceofspadesss.github.io/weather-app/images/clear-sky.jpg'); 
                                                                 props.setGradient({ color1: 'rgba(86,77,241,0.9)', color2: 'rgba(80,80,246,0.8)', color3: 'rgba(219,249,255,0.7)' 
                                                                 })} }>Clear Sky Demo</button>
                             <button className="dropdown-item" onClick={() => {
-                                                                props.setBackground('../images/few-clouds.jpg');
+                                                                props.setBackground('http://aceofspadesss.github.io/weather-app/images/few-clouds.jpg');
                                                                 props.setGradient({ color1: 'rgba(53,98,166,0.9)', color2: 'rgba(68,157,241,0.8)', color3: 'rgba(172,223,233,0.7)' });
                                                                 }}>Few Clouds Demo</button>
                             <button className="dropdown-item" onClick={() => {
-                                                                props.setBackground('../images/scattered-clouds.jpg');
+                                                                props.setBackground('http://aceofspadesss.github.io/weather-app/images/scattered-clouds.jpg');
                                                                 props.setGradient({ color1: 'rgba(136,136,136,0.9)', color2: 'rgba(113,145,175,0.8)', color3: 'rgba(230,230,230,0.7)' });
                                                                 }}>Scattered Clouds Demo</button>
                             <button className="dropdown-item" onClick={() => {
-                                                                props.setBackground('../images/broken-clouds.jpg');
+                                                                props.setBackground('http://aceofspadesss.github.io/weather-app/images/broken-clouds.jpg');
                                                                 props.setGradient({ color1: 'rgba(204,204,204,0.9)', color2: 'rgba(90,134,176,0.8)', color3: 'rgba(29,56,73,0.7)' });
                                                                 }}>Broken Clouds Demo</button>
                             <button className="dropdown-item" onClick={() => {
-                                                                props.setBackground('../images/shower-rain.jpg');
+                                                                props.setBackground('http://aceofspadesss.github.io/weather-app/images/shower-rain.jpg');
                                                                 props.setGradient({ color1: 'rgba(149,177,255,0.9)', color2: 'rgba(40,70,98,0.8)', color3: 'rgba(0,0,0,0.7)' });
                                                                 }}>Shower Rain Demo</button>
                             <button className="dropdown-item" onClick={() => {
-                                                                props.setBackground('../images/rain.jpg');
+                                                                props.setBackground('http://aceofspadesss.github.io/weather-app/images/rain.jpg');
                                                                 props.setGradient({ color1: 'rgba(41,93,21,0.9)', color2: 'rgba(110,150,119,0.8)', color3: 'rgba(231,255,180,0.7)' });
                                                                 }}>Rain Demo</button>
                             <button className="dropdown-item" onClick={() => {
-                                                                props.setBackground('../images/thunderstorm.jpg');
+                                                                props.setBackground('http://aceofspadesss.github.io/weather-app/images/thunderstorm.jpg');
                                                                 props.setGradient({ color1: 'rgba(0,5,45,0.9)', color2: 'rgba(23,21,77,0.8)', color3: 'rgba(145,143,213,0.7)' });
                                                                 }}>Thunderstorm Demo</button>
                             <button className="dropdown-item" onClick={() => {
-                                                                props.setBackground('../images/snow.jpg');
+                                                                props.setBackground('http://aceofspadesss.github.io/weather-app/images/snow.jpg');
                                                                 props.setGradient({ color1: 'rgba(162,170,195,0.9)', color2: 'rgba(233,233,233,0.8)', color3: 'rgba(238,238,238,0.7)' });
                                                                 }}>Snow Demo</button>
                             <button className="dropdown-item" onClick={() => {
-                                                                props.setBackground('../images/mist.jpg');
+                                                                props.setBackground('http://aceofspadesss.github.io/weather-app/images/mist.jpg');
                                                                 props.setGradient({ color1: 'rgba(228,234,240,0.9)', color2: 'rgba(255,255,255,0.8)', color3: 'rgba(164,164,164,0.7)' });
                                                                 }}>Mist Demo</button>
                         </div>
